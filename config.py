3c0959705dfac4b94ee90d66fd67c7a4024cd6cc
Murat <j.murat2020@yandex.ru>
2022-12-10 20:56:13 +0500

    update

--- config.py
+++ config.py
@@ -2,8 +2,22 @@ from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
-    TOKEN: str = ""
-    DATABASE_URL: str = ""
+    TOKEN: str
+    WEBHOOK_URL_PATH: str
+    WEBHOOK_SSL_CERT: str
+    WEBHOOK_SSL_PRIV: str
+
+    SECRET: str
+
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+
+    ADMIN_USERNAME: str
+    ADMIN_EMAIL: str
+    ADMIN_PASSWORD: str
 
     class Config:
         case_sensitive = True
--- database/base.py
+++ database/base.py
@@ -1,12 +1,23 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config import Config
 
-DATABASE_URL = Config().dict().get('DATABASE_URL')
+config = Config()
+
+DATABASE_URL = f"postgresql+asyncpg://{config.POSTGRES_USER}:{config.POSTGRES_PASSWORD}@{config.POSTGRES_HOST}:" \
+               f"{config.POSTGRES_PORT}/{config.POSTGRES_DB}"
+
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base: DeclarativeMeta = declarative_base()
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
 
